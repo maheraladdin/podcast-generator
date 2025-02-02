75e18746d5c2c6698450d0567f9aaf889898dbc5
maheraladdin <maheraladdin@std.mans.edu.eg>
2025-02-02 21:03:20 +0000

    Add entrypoint script to automate application startup and Git operations

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "===> Starting the application \n"
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update feed" && git push --set-upstream origin main
+
+
+echo "===> Exit the application \n"
